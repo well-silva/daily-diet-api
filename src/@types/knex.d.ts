54277ec5912a7065069a59fcb2f6c7df9fc26824
Wellington Silva <well.silvaa97@gmail.com>
2024-01-26 05:31:59 -0300

    feat: add resource meals

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,13 +1,25 @@
-import { Knex } from 'knex'
+import 'knex'
 
 declare module 'knex/types/tables' {
   export interface Tables {
     users: {
       id: string
-      email: string
-      session_id?: string
+      session_id: string
       name: string
+      email: string
       created_at: string
+      updated_at: string
+    }
+
+    meals: {
+      id: string
+      user_id: string
+      name: string
+      description: string
+      is_on_diet: boolean
+      date: number // unix timestamp
+      created_at: string
+      updated_at: string
     }
   }
 }
